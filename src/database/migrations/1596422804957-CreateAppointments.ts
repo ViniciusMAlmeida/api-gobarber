c91ba2ce5026e1dff91a087ce65427bfd101c42e
Vinicius Almeida <vinicius.sec21@gmail.com>
2020-08-04 18:50:20 -0300

    model relationship

--- src/database/migrations/1596422804957-CreateAppointments.ts
+++ src/database/migrations/1596422804957-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1596422804957
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
