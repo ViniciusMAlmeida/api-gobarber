51bcd47ab9533d09b3ebc85f38ac7b7ce4ed63b4
Vinicius Almeida <vinicius.sec21@gmail.com>
2020-08-04 22:59:04 -0300

    token generation

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.')
     }
 
+    const token = sign({}, 'e11e236054bfb4378b22479cf1e459b2', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
     return {
       user,
+      token,
     }
   }
 }
