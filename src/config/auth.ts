4ab98105dd1da86a6e05c078507f1e196216b2af
Vinicius Almeida <vinicius.sec21@gmail.com>
2020-08-05 18:35:41 -0300

    authenticated routes, add user on request

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e11e236054bfb4378b22479cf1e459b2',
+    expiresIn: '1d',
+  },
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 
+import authConfig from '../config/auth'
 import User from '../models/User'
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.')
     }
 
-    const token = sign({}, 'e11e236054bfb4378b22479cf1e459b2', {
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     })
 
     return {
